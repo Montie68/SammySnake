9ff49a3441c00c37d67640660ff30011a71428f7
Unknown <geoff@crystalclearit.com.au>
2017-03-05 00:38:45 +1100

    work end 04/03/2017

--- Assets/Scripts/snake.cs
+++ Assets/Scripts/snake.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public enum PLAYER_DIRECTION
 {
     UP,
     LEFT,
     RIGHT,
     DOWN
+};
+
+public struct SnakeBody
+{
+    GameObject BodyPart;
+    PLAYER_DIRECTION direction;
 };
 
 public class snake : MonoBehaviour {
@@ -20,17 +28,24 @@ public class snake : MonoBehaviour {
         DEAD,
         EAT
     };
+    private SnakeBody[] Body;
+    private SnakeBody[] TempBody;
 
-    
+
+    private PLAYER_STATE PlayerState;
 
     // Use this for initialization
     void Start () {
-		
+        PlayerState = PLAYER_STATE.ALIVE;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (PlayerState == PLAYER_STATE.ALIVE)
+        {
+
+        }
 	}
 
     bool PlayerSetDiretion(PLAYER_DIRECTION direction)
